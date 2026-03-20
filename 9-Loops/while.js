56a3f83aba03c824ea0fd81089463a327dc96ec8
JonathanTubac <javiertubac1290.e@gmail.com>
2026-03-20 01:15:57 -0600

    feat: updated while in section 9

--- 9-Loops/while.js
+++ 9-Loops/while.js
@@ -5,3 +5,10 @@ while(i <= 5) {
     console.log(i)
     i++
 }
+
+//We can use do while loop, which is similar to while loop but it will execute the code block at least once, even if the condition is false
+let j = 1
+do {
+    console.log(j)
+    j++
+} while(j <= 5)
